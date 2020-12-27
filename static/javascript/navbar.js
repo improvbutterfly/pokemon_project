179dda9ef9ada6f7b8401b81267e85718e911ac8
Jeremy Steele <jeremysteele@Jeremys-MacBook-Pro.local>
2020-12-27 12:13:44 -0800

    corrections

--- static/javascript/navbar.js
+++ static/javascript/navbar.js
@@ -5,7 +5,7 @@ document.write('\
         		<a href="/index.html">Home</a>\
         		<a href="/fight.html">Fight</a>\
                 <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/International_Pok%C3%A9mon_logo.svg/2000px-International_Pok%C3%A9mon_logo.svg.png" width="200" height="100" />\
-        		<a href="/evolution.html">Evolution</a>\
+        		<a href="/breed.html">Evolution</a>\
         		<a href="/graph.html">Graph</a>\
             </div>\
         </div>\
